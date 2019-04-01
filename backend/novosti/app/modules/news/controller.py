2506f1feef4f913d27cb256c32e8f7ba4224db69
ToninoK <tonino.kastelan@skole.hr>
2019-04-01 19:21:21 +0200

    Fixed error handling

--- backend/novosti/app/modules/news/controller.py
+++ backend/novosti/app/modules/news/controller.py
@@ -1,6 +1,8 @@
 from flask import abort, jsonify
 from .model import Post
 from bson import ObjectId, json_util
+from bson import errors
+from dataclasses import asdict
 from .utilities import conv_to_date
 
 
@@ -14,10 +16,9 @@ class NewsController:
             if not new_post.str_to_date():
                 return abort(400, "Date format is not correct.")
 
-            res = self.db.insert_one(new_post.to_dict())
+            self.db.insert_one(new_post.to_dict())
             new_post.date_to_str()
-            new_post._id = res.inserted_id
-            return new_post.to_resp()
+            return jsonify(data)
 
         except TypeError:
             return abort(400, "Submitted payload does not meet the right parametres.")
@@ -28,15 +29,15 @@ class NewsController:
 
             if not new_post.str_to_date():
                 return abort(400, "Date format is not correct.")
-            if not new_post._id:
+            if "_id" not in data:
                 return abort(400, "Submitted payload does not have an id.")
 
             status = self.db.update_one(
-                {"_id": new_post._id}, {"$set": new_post.to_dict()}
+                {"_id": ObjectId(data["_id"])}, {"$set": new_post.to_dict()}
             )
-            if not status.acknowledged:
+            if status.matched_count == 0:
                 return abort(400, "The submitted id does not exist.")
-            return new_post.to_resp()
+            return jsonify(data)
 
         except TypeError:
             return abort(400, "Submitted payload does not meet the right parametres.")
@@ -45,27 +46,30 @@ class NewsController:
         try:
             status = self.db.delete_one({"_id": ObjectId(data["_id"])})
 
-            if not status.acknowledged:
+            if status.deleted_count == 0:
                 return abort(400, "The submitted id does not exist.")
 
             return jsonify(data["_id"])
 
-        except KeyError:
-            return abort(400, "Submitted payload does not have an id.")
+        except (KeyError, errors.InvalidId):
+            return abort(
+                400, "Submitted payload does not have a valid id or no id at all."
+            )
 
     def get_data(self, args):
-        keys = args.keys()
 
-        if all(elem in ["gt", "lt"] for elem in keys):
+        if "gt" in args and "lt" in args:
             gt = conv_to_date(args["gt"])
             lt = conv_to_date(args["lt"])
+
             if not (gt and lt):
                 return abort(400, "Invalid arguments.")
+
             query = self.db.find({"date": {"$gt": gt, "$lt": lt}})
             news = [Post(**post) for post in query]
             for post in news:
                 post.date = post.date_to_str()
                 post._id = str(post._id)
 
-            return jsonify([post.to_dict() for post in news])
-
+            return jsonify([asdict(post) for post in news])
+        return jsonify
