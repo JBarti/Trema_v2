3815404ccd27ee7383bec2a4d77e563359e60598
Barty <josip.bartulovic3@gmail.com>
2019-07-18 12:16:42 +0200

    Set up news component Added pre commit validation

--- frontend/trema/src/pages/news/News.jsx
+++ frontend/trema/src/pages/news/News.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import './News.css';
+import './news.css';
+import { MainNavbar, SideNavbar } from '../../components/common/navbar';
+import Block from '../../components/common/block/block';
+
 
 const News = () => (
-  <div />
+  <div>
+    <div className='news__heading'>
+      <div className='heading__flag' />
+    </div>
+    <MainNavbar />
+    <SideNavbar />
+    <div className='news__content'>
+      <div className='content__left' />
+      <div className='content__middle' />
+      <div className='content__right' />
+    </div>
+    <Block />
+  </div>
 );
 
 export default News;
