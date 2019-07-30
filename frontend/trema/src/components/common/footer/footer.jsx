357f2126449f8acf1ea19e562b61ee4327a1140d
ivanParat <52297017+ivanParat@users.noreply.github.com>
2019-07-30 16:26:46 +0200

    Changing css class names

--- frontend/trema/src/components/common/footer/footer.jsx
+++ frontend/trema/src/components/common/footer/footer.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './footer.css';
 
 const Footer0 = () => (
-  <div className='footer footer0'>
-    <div className='height0'>
-      <p className='bold'>III.gimnazija, Split</p>
+  <div className='footer footer--height'>
+    <div className='footer__div--height0'>
+      <p className='footer__div--bigger-text'>III.gimnazija, Split</p>
     </div>
   </div>
 );
 const Footer1 = () => (
   <div className='footer'>
-    <div className='height1'>
+    <div className='footer__div--height1'>
       <p>Matice Hrvatske 11, 21000 Split, Hrvatska</p>
       <p>OIB: 78950283030</p>
       <p>
@@ -25,14 +25,14 @@ const Footer1 = () => (
       <p>+385 21 558 420-Centrala</p>
     </div>
     <div>
-      <p><a className='white' href='https://ocjene.skole.hr/pocetna/prijava/ucenici'>E-Dnevnik za učenike</a></p>
-      <p><a className='white' href='https://ocjene.skole.hr/pocetna/prijava/roditelji'>E-Dnevnik za roditelje</a></p>
-      <p><a className='white' href='https://e-dnevnik.skole.hr/main/login'>E-Dnevnik za profesore</a></p>
+      <p><a className='footer--text-color-white' href='https://ocjene.skole.hr/pocetna/prijava/ucenici'>E-Dnevnik za učenike</a></p>
+      <p><a className='footer--text-color-white' href='https://ocjene.skole.hr/pocetna/prijava/roditelji'>E-Dnevnik za roditelje</a></p>
+      <p><a className='footer--text-color-white' href='https://e-dnevnik.skole.hr/main/login'>E-Dnevnik za profesore</a></p>
     </div>
     <div>
-      <p><a className='white' href='https://loomen.carnet.hr/'>Loomen</a></p>
-      <p><a className='white' href='http://e.trema.hr/'>e-Trema</a></p>
-      <p><a className='white' href='https://webmail.skole.hr/login.php'>CARNet Webmail</a></p>
+      <p><a className='footer--text-color-white' href='https://loomen.carnet.hr/'>Loomen</a></p>
+      <p><a className='footer--text-color-white' href='http://e.trema.hr/'>e-Trema</a></p>
+      <p><a className='footer--text-color-white' href='https://webmail.skole.hr/login.php'>CARNet Webmail</a></p>
     </div>
   </div>
 );
