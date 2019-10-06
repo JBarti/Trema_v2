fc06ed30944e6edb23df889360d2ae804edfd82e
AnteProlic <ante.prolic2002@gmail.com>
2019-10-06 17:41:16 +0200

    Add links to page components

--- frontend/trema/src/components/common/navbar/navbar.jsx
+++ frontend/trema/src/components/common/navbar/navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import styles from './navbar.module.css';
 import styleVariants from './styleVariants';
 import SearchBar from '../searchBar/searchBar';
+
 
 const STYLE_VARIANT_CASES = {
   default: {},
@@ -22,9 +24,9 @@ const Navbar = (props) => {
 
 const MainNavbar = () => (
   <Navbar variant='main'>
-    <p>naslovnica</p>
-    <p>novosti</p>
-    <p>o nama</p>
+    <Link to='' style={{ textDecoration: 'none' }}><p>naslovnica</p></Link>
+    <Link to='/novosti' style={{ textDecoration: 'none' }}><p>novosti</p></Link>
+    <Link to='/aboutUs' style={{ textDecoration: 'none' }}><p>o nama</p></Link>
     <p>informacije</p>
     <p>natječaji</p>
     <div className={styles['search__container']}>
--- frontend/trema/src/index.jsx
+++ frontend/trema/src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './index.css';
 import App from './pages/test/App';
 import Home from './pages/home/home';
@@ -11,9 +15,9 @@ import AboutUs from './pages/aboutUs/aboutUs';
 const routes = (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/test' component={App} />
-      <Route exact path='/novosti' component={News} />
-      <Route exact path='/aboutUs' component={AboutUs} />
+      <Route path='/test' component={App} />
+      <Route path='/novosti' component={News} />
+      <Route path='/aboutUs' component={AboutUs} />
       <Route exact path='' component={Home} />
     </Switch>
   </BrowserRouter>
